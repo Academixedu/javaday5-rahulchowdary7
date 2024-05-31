38c3fbfdcd283709f187595978ad133415d95859
rahulchodary7 <rahulanubolu@gmail.com>
2024-05-31 10:48:50 +0530

    im done sir

---
+++ src/main/Calu.java
@@ -0,0 +1,22 @@
+ package com.example;
+public class Calu {
+
+    public static void main(String[] args) {
+        int a,b,c;
+        a=7;
+        b=5;
+        c=a+b;
+        System.out.println("c" +c);
+        String name ="rahul";
+        sum(a,b);
+        greetMethod(name);
+    }
+    public static void greetMethod(string name){
+        System.out.println("hello" +name);
+    }
+    public static string sum(int a,int b){
+        string sum =""+a+b;
+        return sum;
+    }}
+
+
